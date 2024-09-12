bab91b80946334a8182a806339772aa03db122d3
harish04ss <harishsubramanian4122001@gmail.com>
2024-09-12 09:38:54 +0530

    server setup done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
 const mongoose = require('mongoose');
 const { mongodb_url } = require('./utils/config');
+const app = require('./app');
 
 mongoose.connect(mongodb_url)
-.then(()=> console.log("connected to DB"))
+.then(()=> {console.log("connected to DB");
+    app.listen(3001,()=>{
+        console.log("server is running on port 3001 at http://localhost:3001")
+    });
+})
 .catch(err=> console.error("couldn't connet to DB",err));
